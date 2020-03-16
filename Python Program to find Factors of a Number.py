7e7d35a94a49f764f51a45a3b7ac70476af34d38
SYKON77 <58018484+SYKON77@users.noreply.github.com>
2020-03-16 08:39:54 +0530

    Add files via upload

---
+++ Python Program to find Factors of a Number.py
@@ -0,0 +1,11 @@
+# Python Program to find Factors of a Number
+ 
+number = int(input("Please Enter any Number: "))
+
+value = 1
+print("Factors of a Given Number {0} are:".format(number))
+
+while (value <= number):
+    if(number % value == 0):
+        print("{0}".format(value))
+    value = value + 1
